44e81b00dcd42121528da8c815dd70ce4c89428f
Julio Cesar Lugo Castañeda <a01748403@tec.mx>
2022-04-07 20:53:27 -0500

    actualizacion

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from crypt import methods
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+import datetime
 
 # FlASK
 #############################################################
 app = Flask(__name__)
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+app.secret_key = "super secret key"
 #############################################################
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    email = None
+    if "email" in session:
+        email = session["emai"]
+        return render_template('index.html', error=email)
+    else:
+        return render_template('login.html', error=email)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if(request.method =="GET"):
-        return render_template("login.html", error="email")
+    email = None
+    if "email" in session:
+        return render_template('index.html', data=session["email"])
     else:
-        email = request.form["email"]
-        password = request.form["password"]
-        return render_template("index.html", error=email)
+
+        if(request.method =="GET"):
+            return render_template("login.html", error="email")
+        else:
+            email = request.form["email"]
+            password = request.form["password"]
+            session["email"] = email
+            return render_template("index.html", error=email)
 
 @app.route('/estructuradedatos')
 def prueba():
